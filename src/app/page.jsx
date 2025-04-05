816e66b929abeaa50f245168ccf8007dce6392ea
HoangBaoPhuoc <23521231@gm.uit.edu.vn>
2025-04-05 19:54:40 +0700

    WIP: commit tạm để pull main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,95 +1,44 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/Header/index.jsx";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <Header />
+      <div className={styles.overview}>
+        <div className={styles.overview_background}>
+          {/* Overview Header */}
+          <header className={styles.overview_header}>
+            <div className={styles.overview_header_logohead}>XANGFUTING</div>
+            <div className={styles.overview_header_authButtons}>
+              <div className={styles.overview_header_authButtons_signin}>
+                <button>Đăng nhập</button>
+              </div>
+              <div className={styles.overview_header_authButtons_register}>
+                <button>Đăng ký</button>
+              </div>
+            </div>
+          </header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          {/* Overview Body */}
+          <div className={styles.overview_body}>
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/Img/Homepage/Fulllogo.png"
+              alt="Cum-logo-Doan-Khoa-MMTT"
+              width={459}
+              height={186}
+              className={styles.overview_body_logo}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <h1 className={styles.overview_body_title}>
+              TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÔNG TIN - ĐHQG-HCM
+            </h1>
+            <h2 className={styles.overview_body_subtitle}>
+              ĐOÀN KHOA MẠNG MÁY TÍNH VÀ TRUYỀN THÔNG
+            </h2>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
