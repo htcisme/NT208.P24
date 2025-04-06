96b91f837a63f277cc086dcff67983a70adaa56f
htcisme <23521673@gm.uit.edu.vn>
2025-04-06 17:45:34 +0700

    add Booking page

--- src/app/Booking/page.jsx
+++ src/app/Booking/page.jsx
@@ -7,11 +7,20 @@ export default function Home() {
       {/* Chữ "Đặt lịch" */}
       <div className="booking-header-title">ĐĂNG KÝ ĐẶT PHÒNG</div>
 
+      {/* Hình ảnh đoàn khoa */}
+
+      <Image
+        className="booking-body-img"
+        src="/Img/Booking/tapthedoankhoa.png"
+        alt="the-the-doan-khoa"
+        width={700}
+        height={50}
+      />
       {/* Khung đặt lịch */}
       <iframe
         className="booking-body-frame"
         src="https://docs.google.com/forms/d/e/1FAIpQLSe9000S4USowSCYt3VytTDPzDCCr9wrz6EYhTADJ3jEdkWhIg/viewform?embedded=true"
-        width="750"
+        width="700"
         height="1369"
         frameborder="0"
         marginheight="0"
