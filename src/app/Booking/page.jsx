254997b14d25a970de073d61d597a7b95a5135d9
HoangBaoPhuoc <23521231@gm.uit.edu.vn>
2025-04-10 21:42:55 +0700

    Create basic homepage

--- src/app/Booking/page.jsx
+++ src/app/Booking/page.jsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
 import styles from "./style.css";
 
-export default function Home() {
+export default function Booking() {
   return (
     <div className="booking">
       {/* Chữ "Đặt lịch" */}
       <div className="booking-header-title">ĐĂNG KÝ ĐẶT PHÒNG</div>
 
+      {/* Hình ảnh đoàn khoa */}
+      <div className="booking-body-cropbox">
+        <Image
+          className="booking-body-img"
+          src="/Img/Booking/tapthedoankhoa.jpg"
+          alt="the-the-doan-khoa"
+          width={700}
+          height={50}
+        />
+      </div>
+
       {/* Khung đặt lịch */}
       <iframe
         className="booking-body-frame"
         src="https://docs.google.com/forms/d/e/1FAIpQLSe9000S4USowSCYt3VytTDPzDCCr9wrz6EYhTADJ3jEdkWhIg/viewform?embedded=true"
-        width="750"
+        width="700"
         height="1369"
         frameborder="0"
         marginheight="0"
         marginwidth="0"
-      >
-        Loading…
-      </iframe>
+      />
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import "@/styles-comp/style.css";
 import Image from "next/image";
-
 export default function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    if (savedMode === "true") {
+      setIsDarkMode(true);
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "false");
+    } else {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "true");
+    }
+  };
+
   return (
     <div className="Header">
       <div className="Header-Topbar">
@@ -55,15 +80,66 @@ export default function Header() {
             </span>
           </div>
         </div>
+
+        <button
+          onClick={toggleDarkMode}
+          className="Header-Topbar-Dark-mode-toggle"
+        >
+          {isDarkMode ? (
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          )}
+        </button>
       </div>
 
       <div className="Header-Navbar">
-        <div className="Header-Navbar-Navitem">TRANG CHỦ</div>
-        <div className="Header-Navbar-Navitem">GIỚI THIỆU</div>
-        <div className="Header-Navbar-Navitem">HOẠT ĐỘNG</div>
-        <div className="Header-Navbar-Navitem">THÀNH TÍCH</div>
-        <div className="Header-Navbar-Navitem">ĐẶT PHÒNG</div>
-        <div className="Header-Navbar-Navitem">LIÊN HỆ</div>
+        <div className="Header-Navbar-Navitem">
+          <a href="/">TRANG CHỦ</a>
+        </div>
+        <div className="Header-Navbar-Navitem">
+          <a href="/Introduction">GIỚI THIỆU</a>
+        </div>
+        <div className="Header-Navbar-Navitem">
+          <a href="Activities">HOẠT ĐỘNG</a>
+        </div>
+        <div className="Header-Navbar-Navitem">
+          <a href="/Awards">THÀNH TÍCH</a>
+        </div>
+        <div className="Header-Navbar-Navitem">
+          <a href="/Booking">ĐẶT PHÒNG</a>
+        </div>
+        <div className="Header-Navbar-Navitem">
+          <a href="/Contact">LIÊN HỆ</a>
+        </div>
       </div>
     </div>
   );
