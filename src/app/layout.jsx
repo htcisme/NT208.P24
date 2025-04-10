59fe7d3e1787f8f42fcfab0cb3fc362206ebad01
dkangtext <23520694@gm.uit.edu.vn>
2025-04-10 11:20:18 +0700

    update khangnd/activities

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header></Header>
         {children}
         <Footer></Footer>
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>Trang chính</h1>
-      <Footer />
     </div>
   );
 }
