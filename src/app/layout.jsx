dc0ef7cbc567fb1a0f9291b49fe0faf115cf2707
dkangtext <23520694@gm.uit.edu.vn>
2025-04-14 10:38:52 +0700

    fix conflict layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+
 export const metadata = {
   title: "Trang chủ",
   description: "Tạo bởi XangFuTing",
@@ -9,16 +10,10 @@ export default function Home({ children }) {
   return (
     <html lang="en">
       <body>
-<<<<<<< HEAD
         <Header></Header>
         {children}
         <Footer></Footer>
       </body>
-=======
-        {children}
-      </body>
-
->>>>>>> 6141fd41829e5f0619e66833edbcec3a1a1e0854
     </html>
   );
 }
