832cd12e7ea86a43ba8e2704d9c24e0f7adee27a
dkangtext <23520694@gm.uit.edu.vn>
2025-04-14 10:39:41 +0700

    fix conflict layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,16 +9,10 @@ export default function Home({ children }) {
   return (
     <html lang="en">
       <body>
-<<<<<<< HEAD
         <Header></Header>
         {children}
         <Footer></Footer>
       </body>
-=======
-        {children}
-      </body>
-
->>>>>>> 6141fd41829e5f0619e66833edbcec3a1a1e0854
     </html>
   );
 }
